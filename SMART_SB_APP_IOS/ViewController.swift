d71ecab2605d5f4c4c6d8295627c6f68eb69367b
namkyo <hipen1@naver.com>
2021-01-10 13:42:17 +0900

    Initial Commit

---
+++ SMART_SB_APP_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMART_SB_APP_IOS
+//
+//  Created by 김남교 on 2021/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
